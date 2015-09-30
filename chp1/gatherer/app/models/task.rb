e2d0a3c8ec656804d5c47a386dff5312f52607bb
moeabdol <admin.r99@gmail.com>
2015-09-30 03:45:46 +0300

    initial commit

---
+++ chp1/gatherer/app/models/task.rb
@@ -0,0 +1,25 @@
+class Task
+  attr_accessor :completed_at, :size
+
+  def initialize(options = {})
+    mark_completed(options[:completed_at]) if options[:completed_at]
+    @size = options[:size]
+  end
+
+  def mark_completed(date = nil)
+    @completed_at = date || Time.current
+  end
+
+  def complete?
+    @completed_at.present?
+  end
+
+  def part_of_velocity?
+    return false unless complete?
+    completed_at > Project.velocity_length_in_days.days.ago
+  end
+
+  def points_toward_velocity
+    if part_of_velocity? then size else 0 end
+  end
+end
