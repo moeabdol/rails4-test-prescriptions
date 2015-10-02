597cf42660ced77314572f8a00b8b772863cbb8a
moeabdol <admin.r99@gmail.com>
2015-10-02 22:16:08 +0300

    add factory_girl_rails gem

--- chp5/gatherer/spec/rails_helper.rb
+++ chp5/gatherer/spec/rails_helper.rb
@@ -49,4 +49,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # Include FactoryGirl methods is specs
+  config.include FactoryGirl::Syntax::Methods
 end
